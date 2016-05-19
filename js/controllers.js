34f45452ac292a1131d2ac1ddc51be5a5570e936
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2016-05-19 17:17:18 +0200

    Adjust to changed api.

--- js/controllers.js
+++ js/controllers.js
@@ -16,7 +16,7 @@ angular.module('chronontology.controllers', [])
 
 .controller("SearchController", function($scope, $location, $http) {
 
-	$http.get('/data/period/?q=' + $location.search()['q']).success(function(result) {
+	$http.get('/data/period/?' + $location.search()['q']).success(function(result) {
 		$scope.periods = result.results;
 	});
 
