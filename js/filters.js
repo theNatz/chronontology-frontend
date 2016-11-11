fd3215e3d578703edecdb6e0068887c70afcc570
Wolfgang Schmidle <wolfgang.schmidle@dainst.de>
2016-11-11 14:19:45 +0100

    added prefName filter

--- js/filters.js
+++ js/filters.js
@@ -7,3 +7,36 @@ angular.module('chronontology.filters', [])
 			return new Date(dateString);
 		}
 	})
+	
+	.filter('prefName', function() {
+
+		return function(names) {
+
+			// if names is non-empty then we assume there is 
+			// at least one name for each given language
+
+			var browserlang = 'en';
+
+			var availableLanguages = names.map(function(line) { return line.lang; });
+			var browserlangPosition = availableLanguages.indexOf(browserlang);
+			var enPosition = availableLanguages.indexOf('en');
+
+			if(names.length > 0) {
+				if (browserlangPosition > -1) {
+					// user's preferred language is available
+					return names[browserlangPosition].content[0];
+				}
+				else if (enPosition > -1) {
+					// at least English is available
+					return names[enPosition].content[0];
+				}
+				else {
+					// at least any language is available
+				    return names[0].content[0];
+				}
+			}
+			else {
+			    return "(kein Name)";
+			}			
+		}
+	})
