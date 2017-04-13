ed3e0742f5afa055f6aa5d3f8ad227bff3e0fb00
Simon Hohl <mail@dersmon.net>
2017-04-13 10:19:57 +0200

    Fixed filter error when applied to 'undefined' value.

--- js/filters/prefName.js
+++ js/filters/prefName.js
@@ -5,17 +5,16 @@ angular.module('chronontology.filters')
 	.filter('prefName',['language', function(language) {
 
 		return function(names) {
-
-			// if names is non-empty then we assume there is 
+			// if names is non-empty then we assume there is
 			// at least one name for each given language
 
-			var browserlang = language.browserPrimaryLanguage();
-			var availableLanguages = names.map(function(line) { return line.lang; });
-			var browserlangPosition = availableLanguages.indexOf(browserlang);
-			var enPosition = availableLanguages.indexOf('en');
+			if(names != undefined && names.length > 0) {
+                var browserlang = language.browserPrimaryLanguage();
+                var availableLanguages = names.map(function(line) { return line.lang; });
+                var browserlangPosition = availableLanguages.indexOf(browserlang);
+                var enPosition = availableLanguages.indexOf('en');
 
-			if(names.length > 0) {
-				if (browserlangPosition > -1) {
+                if (browserlangPosition > -1) {
 					// user-preferred language is available
 					return names[browserlangPosition].content[0];
 				}
