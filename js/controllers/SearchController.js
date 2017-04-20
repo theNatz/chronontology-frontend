9b4031f15b870de53c94d448719901f36ef75986
Sebastian Cuy <sebastian.cuy@dainst.de>
2017-04-20 16:38:38 +0200

    add column for region in search result closes #3111

--- js/controllers/SearchController.js
+++ js/controllers/SearchController.js
@@ -4,4 +4,19 @@ angular.module('chronontology.controllers')
 
 .controller("SearchController", function($scope, $location) {
 
+    $scope.getRegion = function(period) {
+
+        if (!period.derived) {
+            return '-';
+        } else if(period.derived.namedAfter) {
+            return period.derived.namedAfter[0].prefName.title;
+        } else if(period.derived.hasCoreRegion) {
+            return period.derived.hasCoreRegion[0].prefName.title;
+        } else if(period.derived.spatiallyPartOfRegion) {
+            return period.derived.spatiallyPartOfRegion[0].prefName.title;
+        } else {
+            return '-';
+        }
+    }
+
 })
