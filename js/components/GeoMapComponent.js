2a3deb28a3cd6b66bcc9987b6af92d7b9559b4c9
Florian Thiery <web@florian-thiery.de>
2017-05-11 11:50:59 +0200

    geowidget geom legend style modified

--- js/components/GeoMapComponent.js
+++ js/components/GeoMapComponent.js
@@ -103,11 +103,11 @@ angular.module('chronontology.components')
                         "<span><b>" + props.name + "</b></span>" +
                         "<br><span>relation: " + props.relation + "</span>" +
                         "<br><span>gazetteerID: " + props.id + "</span>" +
-                        "<br><span>parent geometry: " + props.parentGeometry.name + "</span>" +
+                        "<br><hr class='linehr'/><span>parent geometry: " + props.parentGeometry.name + "</span>" +
                         "<br><span>parent ID: " + props.parentGeometry.id + "</span>"
                         : "<span>no geometry selected</span>");
-                    if (this._div.innerHTML.indexOf("<br><span>parent geometry: geom origin</span><br><span>parent ID: null</span>") != -1) {
-                        this._div.innerHTML = this._div.innerHTML.replace("<br><span>parent geometry: geom origin</span><br><span>parent ID: null</span>","");
+                    if (this._div.innerHTML.indexOf("<br><hr class='linehr'/><span>parent geometry: geom origin</span><br><span>parent ID: null</span>") != -1) {
+                        this._div.innerHTML = this._div.innerHTML.replace("<br><hr class='linehr'/><span>parent geometry: geom origin</span><br><span>parent ID: null</span>","");
                     }
                 };
                 _this.infoLegend.addTo(_this.map);
