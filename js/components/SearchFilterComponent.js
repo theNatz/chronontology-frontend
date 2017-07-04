0a84a5e8b3a39618b8768242d974447760ca0a91
Wolfgang Schmidle <wolfgang.schmidle@dainst.de>
2017-07-04 17:37:39 +0200

    move filters to separate components

---
+++ js/components/SearchFilterComponent.js
@@ -0,0 +1,53 @@
+angular.module('chronontology.components')
+    .component('searchFilter',{
+        templateUrl: '../../partials/search/filter.html',
+        bindings:
+        {
+            facette: '<',
+            facets: '<',
+            query: '<'
+        },
+        controller: function() {
+            this.getFacetValues = function(){
+                // aus der Frontend-URI: ohne "resource."
+                var fq = this.query.fq;
+                if (fq == null) return "";
+                if (typeof fq === 'string') return "&fq="+fq;
+                return "&fq=" + fq.join("&fq=");
+            }
+
+            this.getFacetValuesExcept = function(facette, facetvalue){
+                var fq = this.query.fq;
+                var facettenwert = facette + ":" + facetvalue;
+                // 1. sowieso kein Facettenwert ausgewählt?
+                if (fq == null) return "";
+                // 2. genau ein Facettenwert ausgewählt?
+                if (typeof fq === 'string') {
+                    if (facettenwert === fq) {
+                        return "";
+                    } else {
+                        return "&fq="+fq;
+                    }
+                }
+                // 3. mehr als ein Facettenwert ausgewählt?
+                if (fq.indexOf(facettenwert) > -1) {
+                    var fqOhne = angular.copy(fq)
+                    fqOhne.splice(fq.indexOf(facettenwert), 1);
+                    return "&fq=" + fqOhne.join("&fq=");
+                } else {
+                    return "&fq=" + fq.join("&fq=");
+                }
+            }
+            this.isFacetValueSelected = function(facette, facetvalue){
+                var zusammen = facette + ":" + facetvalue;
+                var fq = this.query.fq;
+                if (fq == null) return false;
+                if (typeof fq === 'string') return (zusammen === fq);
+                return (fq.indexOf(zusammen) > -1);
+            }
+            this.addResource = function(text){
+                return "resource." + text;
+            }
+
+        }
+    });
--- js/controllers/SearchController.js
+++ js/controllers/SearchController.js
@@ -32,44 +32,6 @@ angular.module('chronontology.controllers')
         return Math.ceil($scope.total / $scope.query.size);
     }
 
-    $scope.getFacetValues = function(){
-        // aus der Frontend-URI: ohne "resource."
-        var fq = $scope.query.fq;
-        if (fq == null) return "";
-        if (typeof fq === 'string') return "&fq="+fq;
-        return "&fq=" + fq.join("&fq=");
-    }
-
-    $scope.getFacetValuesExcept = function(facette, facetvalue){
-        var fq = $scope.query.fq;
-        var facettenwert = facette + ":" + facetvalue;
-        // 1. sowieso kein Facettenwert ausgewählt?
-        if (fq == null) return "";
-        // 2. genau ein Facettenwert ausgewählt?
-        if (typeof fq === 'string') {
-            if (facettenwert === fq) {
-                return "";
-            } else {
-                return "&fq="+fq;
-            }
-        }
-        // 3. mehr als ein Facettenwert ausgewählt?
-        if (fq.indexOf(facettenwert) > -1) {
-            var fqOhne = angular.copy(fq)
-            fqOhne.splice(fq.indexOf(facettenwert), 1);
-            return "&fq=" + fqOhne.join("&fq=");
-        } else {
-            return "&fq=" + fq.join("&fq=");
-        }
-    }
-    $scope.isFacetValueSelected = function(facette, facetvalue){
-        var zusammen = facette + ":" + facetvalue;
-        var fq = $scope.query.fq;
-        if (fq == null) return false;
-        if (typeof fq === 'string') return (zusammen === fq);
-        return (fq.indexOf(zusammen) > -1);
-    }
-
     $scope.addResource = function(text){
         return "resource." + text;
     }
