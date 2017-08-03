a2acf0785063b72ff689bc72a854378f2034928c
Wolfgang Schmidle <wolfgang.schmidle@dainst.de>
2017-08-03 15:46:54 +0200

    transl8Number2, temporarily

--- js/filters/prefName.js
+++ js/filters/prefName.js
@@ -31,4 +31,23 @@ angular.module('chronontology.filters')
 			    return "(kein Name)";
 			}
 		}
-	}]);
+	}])
+	.filter('transl8Number2', ['language',function(language){
+
+        var filterFunction = function(nu) {
+
+            if (typeof nu == 'undefined') return undefined;
+
+			if (typeof nu === 'string' && isNaN(nu)) return nu;
+
+			var num = (typeof nu === 'string' && !isNaN(nu)) ? +nu : nu;
+
+            if (language.currentLanguage()==COMPONENTS_GERMAN_LANG) {
+                return num.toLocaleString(COMPONENTS_GERMAN_LANG+"-DE");
+            } else {
+                return num.toLocaleString(COMPONENTS_ENGLISH_LANG+"-US");
+            }
+        };
+        filterFunction.$stateful=true;
+        return filterFunction;
+    }]);
