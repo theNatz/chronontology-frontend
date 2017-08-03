78c692a51013ec64e2945b4c96ce7370bbe24071
Wolfgang Schmidle <wolfgang.schmidle@dainst.de>
2017-08-03 11:12:46 +0200

    repair request URL for thesaurus view

--- js/controllers/ThesaurusController.js
+++ js/controllers/ThesaurusController.js
@@ -3,8 +3,13 @@
 angular.module('chronontology.controllers')
 
 .controller("ThesaurusController", function($scope, $routeParams, $http) {
-		
-	$http.get('/data/period/?size=1000&q=provenance:' + $routeParams.provenance).success( function(result) {
+
+	var relevantParts =
+		'resource.id,resource.names,resource.hasTimespan,'+
+		'resource.relations.isPartOf,resource.relations.hasPart,'+
+		'resource.relations.follows,resource.relations.isFollowedBy';
+
+	$http.get('/data/period/?size=1000&q=resource.provenance:' + $routeParams.provenance + '&part='+relevantParts).success( function(result) {
 		$scope.periods = result.results;
 	});
 })
