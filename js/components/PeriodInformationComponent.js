c956e0f8392c63e50b5074cc64d6ec7c873383ff
Wolfgang Schmidle <wolfgang.schmidle@dainst.de>
2017-10-20 14:45:48 +0200

    do not display superfluous headings

--- js/components/PeriodInformationComponent.js
+++ js/components/PeriodInformationComponent.js
@@ -12,5 +12,36 @@ angular.module('chronontology.components')
         },
         controller: function (authService) {
             this.authService = authService
+
+            this.hasRelationsInResource = function(){
+                return period.hasOwnProperty('relations');
+            }
+            this.hasRelationsInDerived = function(){
+                return (document.hasOwnProperty('derived') &&
+                        document.derived.hasOwnProperty('relations'));
+            }
+            this.hasGazetteerRelationsInDerived = function(){
+                if (!document.hasOwnProperty('derived')) {
+                    return false;
+                }
+                for (var i in $scope.gazetteerRelationTypes) {
+                    var currentType = $scope.gazetteerRelationTypes[i];
+                    if (document.hasOwnProperty(currentType)) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            this.hasTimespanInDerived = function(){
+                return (document.hasOwnProperty('derived') &&
+                        document.derived.hasOwnProperty('hasTimespan'));
+                return ();
+            }
+            this.hasDerivedInformation = function(){
+                return (this.hasGazetteerRelationsInDerived() ||
+                        this.hasTimespanInDerived() ||
+                        this.hasRelationsInDerived());
+            }
+
         }
     });
