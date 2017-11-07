f1512e74d39733a458e175f5c50a64de00d2e8d5
Florian Thiery <web@florian-thiery.de>
2017-11-07 11:27:32 +0100

    enable bbox search for getty

--- js/components/GeoSearchComponent.js
+++ js/components/GeoSearchComponent.js
@@ -3,38 +3,42 @@ function GeoSearchController($scope, $location, $routeParams, $http, $sce, chron
     var _this = this;
     var popupinfo = [];
 
-    _this.loading = true;
+    _this.loading = false;
     _this.empty = false;
 
     this.$onChanges = function(changes) {
         if (changes.selectedPeriodId && _this.selectedPeriodId) {
             this.initMap();
-            _this.loadData();
         }
     };
 
-    this.loadData = function() {
-        $http.get("/spi/place?bbox=50.082665;8.161050;50.082665;8.371850;49.903887;8.161050;49.903887;8.371850&type=dai", {
+    this.loadData = function(upperright, upperleft, lowerleft, lowerright, e) {
+        var bbox = upperleft + ";" + lowerleft + ";" + upperright + ";" + lowerright;
+        console.log("http://ls-dev.i3mainz.hs-mainz.de/spi/place?bbox="+bbox+"&type=getty");
+        _this.loading = true;
+        _this.empty = true;
+        $http.get("/spi/place?bbox="+bbox+"&type=getty", {
                 headers: { 'Authorization': undefined }
         }).then(function success(geojson){
-            console.log(geojson.data);
             _this.loading = false;
             _this.geojson = geojson.data;
-            console.log(geojson.data.features.length);
-            if (geojson.data.features.length > 0) _this.initPlaces(geojson.data);
-            else _this.empty = true;
+            if (geojson.data.features.length > 0) {
+                _this.empty = false;
+                _this.initPlaces(geojson.data);
+            } else {
+                _this.empty = true;
+            }
         }, function error(err) {
             console.warn(err) // TODO show error message
         });
     };
 
     this.initMap = function() {
-
         // init map
-        _this.mapY = 0;
-        _this.mapX = 0;
+        _this.mapY = 50.1287762;
+        _this.mapX = -5.4532871;
         _this.markersArea = 0;
-        _this.mapZoom = 1;
+        _this.mapZoom = 8;
         _this.baseLayer = L.tileLayer('http://{s}.tiles.mapbox.com/v3/isawnyu.map-knmctlkh/{z}/{x}/{y}.png', {
             maxZoom: 15,
             attribution: "<a href='javascript:alert(\"Tiles (c) MapBox (http://mapbox.com), Data (c) OpenStreetMap (http://www.openstreetmap.org) and contributors, CC-BY-SA, Tiles and Data (c) 2013 AWMC (http://www.awmc.unc.edu) CC-BY-NC 3.0 (http://creativecommons.org/licenses/by-nc/3.0/deed.en_US)\");'>Attribution</a>"
@@ -45,22 +49,66 @@ function GeoSearchController($scope, $location, $routeParams, $http, $sce, chron
             zoom: _this.mapZoom,
             layers: [_this.baseLayer]
         });
-        // disable interaction
-        _this.map.dragging.disable();
-        _this.map.touchZoom.disable();
-        _this.map.doubleClickZoom.disable();
-        _this.map.scrollWheelZoom.disable();
-        _this.map.boxZoom.disable();
-        _this.map.keyboard.disable();
-        if (_this.map.tap) _this.map.tap.disable();
-        document.getElementById('map').style.cursor = 'default';
-
+        // add scale
+        L.control.scale().addTo(_this.map);
+        // add draw control
+		_this.drawnItems = new L.FeatureGroup();
+		L.drawLocal.draw.toolbar.buttons.rectangle = 'Draw a boundingbox for selection';
+		_this.drawControl = new L.Control.Draw({
+			position: 'topleft',
+			draw: {
+				polyline: false,
+				polygon: false,
+				circle: false,
+                circlemarker: false,
+				marker: false,
+				rectangle: {
+					metric: true,
+					shapeOptions: {
+						color: '#0000FF'
+					}
+				}
+			},
+			edit: {
+				featureGroup: _this.drawnItems,
+				remove: false,
+				edit: false
+			}
+		});
+		_this.map.addControl(_this.drawControl);
+        // add bbox action
+        _this.map.on('draw:created', function (e) {
+            if (_this.markers) {
+                _this.map.removeLayer(_this.markers);
+            }
+            if (e.layer._latlngs) {
+                var maxAreaSize = 25000;
+                var upperright = e.layer._latlngs[0].lat + ";" + e.layer._latlngs[0].lng;
+    			var upperleft = e.layer._latlngs[1].lat + ";" + e.layer._latlngs[1].lng;
+    			var lowerleft = e.layer._latlngs[2].lat + ";" + e.layer._latlngs[2].lng;
+    			var lowerright = e.layer._latlngs[3].lat + ";" + e.layer._latlngs[3].lng;
+    			var x = ((e.layer._latlngs[0].lng) + (e.layer._latlngs[3].lng)) / 2;
+    			var y = ((e.layer._latlngs[0].lat) + (e.layer._latlngs[2].lat)) / 2;
+    			var area = (LGeo.area(e.layer) / 1000000).toFixed(2);
+    			var currentZoom = _this.map.getZoom();
+    			if (area < maxAreaSize) {
+                    _this.loadData(upperright, upperleft, lowerleft, lowerright, e);
+    				setTimeout(function () {
+    					_this.map.setView([y, x], 8);
+    				}, 1);
+    			} else {
+                    alert("selected area is too big: " + area + " km2");
+    				setTimeout(function () {
+    					_this.map.setView([y, x], currentZoom);
+    				}, 1);
+    			}
+    		} else {
+    			console.log("latlng=" + e.layer._latlng.lat + ";" + e.layer._latlng.lng);
+    		}
+    	});
     };
 
     this.initPlaces = function(geojson) {
-
-        // add scale
-        L.control.scale().addTo(_this.map);
 
         // cluster
         _this.markers = null;
@@ -84,13 +132,6 @@ function GeoSearchController($scope, $location, $routeParams, $http, $sce, chron
         // zoom to bounds
         _this.map.fitBounds(_this.marker.getBounds());
 
-        // enable interaction
-        _this.map.dragging.enable();
-        _this.map.touchZoom.enable();
-        _this.map.doubleClickZoom.enable();
-        _this.map.scrollWheelZoom.enable();
-        _this.map.boxZoom.enable();
-        _this.map.keyboard.enable();
         if (_this.map.tap) _this.map.tap.enable();
         document.getElementById('map2').style.cursor='grab';
     }
