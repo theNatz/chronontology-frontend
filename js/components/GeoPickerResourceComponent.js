1868c1ec548d156e6ddd5c811192b5f036d591dd
Florian Thiery <web@florian-thiery.de>
2017-11-24 08:25:45 +0100

    add GeoPickerResourceComponent

---
+++ js/components/GeoPickerResourceComponent.js
@@ -0,0 +1,44 @@
+angular.module('chronontology.components')
+.controller('GeoPickerResourceModalController', function($uibModalInstance, $http) {
+
+        var _this = this;
+
+        this.loadPlace = function(type,id) {
+            $http.get("/spi/place/"+type+"/"+id, {
+                headers: { 'Authorization': undefined }
+            }).then(function success(geojson){
+                var uri = geojson.data.properties["@id"];
+                console.log(uri);
+            });
+        };
+
+});
+
+function GeoPickerResourceController($http, $uibModal) {
+
+    var _this = this;
+
+    this.openModal = function() {
+        var modal = $uibModal.open({
+			templateUrl: "geopickerResource_modal.html",
+			controller: "GeoPickerResourceModalController",
+			bindToController: true,
+			size: 'lg',
+            controllerAs: '$ctrl'
+		});
+		modal.result.then(function(item) {
+            // TODO: set URI in textfield
+            //_this.onPlaceSelected({place: item});
+		});
+    }
+
+}
+
+angular.module('chronontology.components')
+    .component('geopickerresource',{
+        templateUrl: '../../partials/geo/pickerResource.html',
+        bindings: {
+            onPlaceSelected: '&'
+        },
+        controller: GeoPickerResourceController
+    });
