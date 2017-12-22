e24379f9ebfd60a4797f9a4c7aec402e68d77b3c
Thomas Kleinke <thomas.kleinke@dainst.de>
2017-12-22 01:37:28 +0100

    add relation type dropdown to relation editor & only show one idai picker per relation category

--- js/components/PeriodEditComponent.js
+++ js/components/PeriodEditComponent.js
@@ -18,9 +18,14 @@ angular.module('chronontology.components')
             _this.validTypes = chronontologySettings.validTypes;
             _this.validSubtypes = chronontologySettings.validSubtypes;
             _this.validProvenances = chronontologySettings.validProvenances;
-            _this.internalRelations = chronontologySettings.internalRelationTypes;
+
+            _this.internalRelationTypes = chronontologySettings.internalRelationTypes;
             _this.allenRelationTypes = chronontologySettings.allenRelationTypes;
             _this.gazetteerRelationTypes = chronontologySettings.gazetteerRelationTypes;
+
+            _this.chosenInternalRelationType = _this.internalRelationTypes[0];
+            _this.chosenAllenRelationType = _this.allenRelationTypes[0];
+            _this.chosenGazetteerRelationType = _this.gazetteerRelationTypes[0];
 
             _this.period = null;
             _this.relatedPeriods = null;
@@ -33,8 +38,8 @@ angular.module('chronontology.components')
             _this.pickedRelations = function(){
                 var result = {};
 
-                for(var i = 0; i < _this.internalRelations.length; i++){
-                    result[_this.internalRelations[i]] = null;
+                for(var i = 0; i < _this.internalRelationTypes.length; i++){
+                    result[_this.internalRelationTypes[i]] = null;
                 }
 
                 for(var i = 0; i < _this.allenRelationTypes.length; i++){
@@ -188,13 +193,16 @@ angular.module('chronontology.components')
                 _this.resourceCache[_this.pickedRelations[relationName].resource.id] = angular.copy(_this.pickedRelations[relationName].resource);
                 _this.pickedRelations[relationName] = null;
             };
+
             _this.removeRelation = function(relationName, relation) {
+
                 _this.period.relations[relationName] = _this.period.relations[relationName].filter(function(x){
                     return x !== relation;
                 });
             };
 
             _this.addGazetteerRelation = function(relationName) {
+
                 if(_this.pickedLocations[relationName] === null) {
                     return;
                 }
@@ -208,6 +216,7 @@ angular.module('chronontology.components')
 
                 _this.pickedLocations[relationName] = null;
             };
+
             _this.removeGazetteerRelation = function(relationName, relation) {
                 _this.period[relationName] = _this.period[relationName].filter(function(x){
                     return x !== relation;
