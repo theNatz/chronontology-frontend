083f06a0698915687c9ac19256081cf1ad5b4b7d
Sebastian Cuy <sebastian.cuy@dainst.de>
2018-10-24 14:48:51 +0200

    Move example query component partial

--- js/components/ExampleQueriesComponent.js
+++ js/components/ExampleQueriesComponent.js
@@ -1,6 +1,6 @@
 angular.module('chronontology.components')
     .component('exampleQueries',{
-        templateUrl: '../../partials/example-queries.html',
+        templateUrl: '../../partials/components/example-queries.html',
         controller: function (chronontologySettings) {
 
             this.queries = chronontologySettings.exampleQueries;
