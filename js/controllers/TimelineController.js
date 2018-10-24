0de65c6688f82df31eae1633dcc96c2747dec65b
Sebastian Cuy <sebastian.cuy@dainst.de>
2018-10-24 15:51:54 +0200

    Add filters to timeline

--- js/controllers/TimelineController.js
+++ js/controllers/TimelineController.js
@@ -2,12 +2,15 @@
 
 angular.module('chronontology.controllers')
 
-.controller("TimelineController", function($scope, $location, searchService, Query) {
+.controller("TimelineController", function($scope, $location, searchService, Query, chronontologySettings) {
 
 	var relevantParts =
 		'resource.id,resource.names,resource.hasTimespan,'+
 		'resource.relations.isPartOf,resource.relations.hasPart,'+
 		'resource.relations.follows,resource.relations.isFollowedBy';
+
+    $scope.existsList = chronontologySettings.existsList;
+    $scope.facetList = chronontologySettings.facetList;
 
 	$scope.query = Query.fromSearch($location.search())
 		.setSize(1000)
