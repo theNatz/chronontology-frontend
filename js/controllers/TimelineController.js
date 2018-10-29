bcfec30cb11117ff0487a1559f40eb156a7f2abe
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2018-10-29 13:23:28 +0100

    Add message if result too large for timeline

--- js/controllers/TimelineController.js
+++ js/controllers/TimelineController.js
@@ -2,7 +2,9 @@
 
 angular.module('chronontology.controllers')
 
-.controller("TimelineController", function($scope, $location, searchService, Query, chronontologySettings) {
+.controller("TimelineController", function($scope, $location, searchService, Query, chronontologySettings, messageService) {
+
+	var MAX_RESULTS = 1000;
 
 	var relevantParts =
 		'resource.id,resource.names,resource.hasTimespan,'+
@@ -13,12 +15,17 @@ angular.module('chronontology.controllers')
     $scope.facetList = chronontologySettings.facetList;
 
 	$scope.query = Query.fromSearch($location.search())
-		.setSize(1000)
+		.setSize(MAX_RESULTS)
 		.setParts(relevantParts);
 
     searchService.search($scope.query).then(function(result){
         $scope.periods = result.results;
         $scope.total = result.total;
         $scope.facets = result.facets;
+
+		if ($scope.total > MAX_RESULTS) {
+			messageService.add("ui_timeline_greater_than_max", "info", false);
+		}
+
     });
 })
